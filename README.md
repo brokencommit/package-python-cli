796526de4d7369aabd326439ccf5a54093d5c764
jatr6994 <JacobTran1996@gmail.com>
2020-03-06 00:30:34 -0700

    better documentation

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
-# Wanted to play around with packaging python scripts into CLI
+# Packaging Python Into CLI Commands
 
 ### Install
 ```
 python setup.py install
 ```
 
-### Run from any where
+### Now You Have A CLI Command!
 ```
 key word is "fudo" for this package
 
 fudo --verbose
 fudo --sushi
 fudo --spell
+```
+
+### How It Works
+```
+- package is built
+- arge parser calls the appropriate function
+- see setup.py to see how to set commands
+* Packages are named poorly for simplicity
 ```
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='package-python-cli',
-      version='practice',
+      version='0.01',
       description='test making cli python script',
       author='Broken Commit',
       github='brokencommit',
       packages=['important'],
       entry_points={
-          'console_scripts': ['fudo=important.test:main'], # fudo is our cli key word
+          'console_scripts': ['fudo=important.test:main'], # "fudo" calls important/test.py => main()
       },
       include_package_data=True,
 )
